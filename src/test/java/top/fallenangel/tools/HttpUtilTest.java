34dbdf9615355877f5fcbc7cf621c842cff9dad6
fallenangel <the.fallenangel.965@gmail.com>
2022-01-05 09:49:50 +0800

    Initial commit

---
+++ src/test/java/top/fallenangel/tools/HttpUtilTest.java
@@ -0,0 +1,18 @@
+package top.fallenangel.tools;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HttpUtilTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
